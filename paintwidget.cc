d7fef43c8fa24db12b8533b24a57ae4cb7eebd8e
Kuntal Majumder <hellozee@disroot.org>
2019-12-31 07:39:21 +0530

    the text can be moved within the canvas

--- paintwidget.cc
+++ paintwidget.cc
@@ -5,7 +5,9 @@
 
 PaintWidget::PaintWidget(QWidget *parent):
     QWidget(parent)
-{ }
+{
+    m_handleRects.resize(4);
+}
 
 void PaintWidget::paintEvent(QPaintEvent *event)
 {
@@ -21,12 +23,18 @@ void PaintWidget::paintEvent(QPaintEvent *event)
 
     auto ascent = m_props[0].font.ascent();
     auto descent = m_props[0].font.descent();
-    qreal displacement = ascent;
+    qreal displacement = ascent, maxWidth = 0;
+
+    for(int i=0;i<m_props.count(); i++){
+        auto glyphWidth = m_props[i].glyph.boundingRect().width();
+        if(maxWidth < glyphWidth)
+            maxWidth = glyphWidth;
+    }
 
     for(int i=0; i<m_props.count(); i++){
         qreal alignment = 1;
+        auto canvasWidth = maxWidth;
         auto glyphWidth = m_props[i].glyph.boundingRect().width();
-        auto canvasWidth = event->rect().width();
 
         switch(m_align){
         case 0 : break;
@@ -40,9 +48,15 @@ void PaintWidget::paintEvent(QPaintEvent *event)
         default: break;
         }
 
-        gc.drawGlyphRun(QPointF(alignment, displacement + 1), m_props[i].glyph);
+        gc.drawGlyphRun(QPointF(alignment, displacement + 1) + m_topLeft, m_props[i].glyph);
         displacement += ascent + descent + m_lineHeight;
     }
+
+    displacement = displacement - (ascent + m_lineHeight) + 1;
+
+    m_boundingRect = QRect(QPoint(m_topLeft - QPoint(-1, 0)), QSize(maxWidth+2, displacement));
+
+    drawHandles(gc);
 }
 
 void PaintWidget::updateWidget(QVector<PropertyHolder> props, qreal lineHeight, int align, QColor col)
@@ -52,4 +66,55 @@ void PaintWidget::updateWidget(QVector<PropertyHolder> props, qreal lineHeight, 
     m_align = align;
     m_textColor = col;
     this->update();
+}
+
+void PaintWidget::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() != Qt::LeftButton)
+        return;
+    if(m_boundingRect.contains(event->pos())){
+        m_lastCursorPos = event->pos() - m_topLeft;
+        m_moveIt = true;
+    }
+}
+
+void PaintWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    if(m_moveIt){
+        m_topLeft = event->pos() - m_lastCursorPos;
+        update();
+    }
+}
+
+void PaintWidget::mouseReleaseEvent(QMouseEvent *event)
+{
+    Q_UNUSED(event)
+    m_moveIt = false;
+}
+
+void PaintWidget::drawHandles(QPainter &gc)
+{
+    gc.drawRect(m_boundingRect);
+
+    QRect handleBox(QPoint(), QSize(5,5));
+
+    handleBox.moveCenter(m_topLeft);
+    gc.fillRect(handleBox, Qt::black);
+
+    m_handleRects[0] = handleBox;
+
+    handleBox.moveCenter(m_boundingRect.topRight());
+    gc.fillRect(handleBox, Qt::black);
+
+    m_handleRects[1] = handleBox;
+
+    handleBox.moveCenter(m_boundingRect.bottomRight());
+    gc.fillRect(handleBox, Qt::black);
+
+    m_handleRects[2] = handleBox;
+
+    handleBox.moveCenter(m_boundingRect.bottomLeft());
+    gc.fillRect(handleBox, Qt::black);
+
+    m_handleRects[3] = handleBox;
 }
--- paintwidget.h
+++ paintwidget.h
@@ -12,11 +12,22 @@ public:
     void updateWidget(QVector<PropertyHolder> props, qreal lineHeight, int align, QColor col);
     void paintEvent(QPaintEvent *event) override;
 
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private:
+    void drawHandles(QPainter &gc);
+
 private:
     QVector<PropertyHolder> m_props;
     qreal m_lineHeight;
     int m_align;
     QColor m_textColor;
+    QPoint m_topLeft = QPoint(0,0), m_lastCursorPos;
+    QRect m_boundingRect;
+    bool m_moveIt = false;
+    QVector<QRect> m_handleRects;
 };
 
 #endif // PAINTWIDGET_H
